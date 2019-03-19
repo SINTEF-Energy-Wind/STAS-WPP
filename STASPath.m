b459f138e8a5daeec63f5a88262d068b58b548f2
SINTEF-Wind-Software <karl.merz@sintef.no>
2019-03-19 19:17:45 +0100

    Add files via upload

---
+++ STASPath.m
@@ -0,0 +1,10 @@
+addpath('STAS-Actuator');
+addpath('STAS-Aeroelastic');
+addpath('STAS-Control');
+addpath('STAS-Electric');
+addpath('STAS-Main');
+addpath('STAS-Test');
+addpath('STAS-Utilities');
+addpath('STAS-Wave');
+addpath('STAS-Wind');
+addpath('STAS-WPP');
