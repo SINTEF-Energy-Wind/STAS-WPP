1e1d3754039f32fda5a9d62581219ce4543add1c
SINTEF-Energy-Wind <karl.merz@sintef.no>
2020-11-30 12:22:48 +0100

    Update prepareElementInput.m

--- STAS-Aeroelastic/prepareElementInput.m
+++ STAS-Aeroelastic/prepareElementInput.m
@@ -1,10 +1,15 @@
 function [mu,dmu,d2mu,TsB,dTsB,d2TsB] = ...
-                  prepareElementInput (qB,qn1,qn2,PB,Pn1,Pn2);
+                  prepareElementInput (linFlag,qB,qn1,qn2,PB,Pn1,Pn2);
 
 [xe,TsB] = elementCSFromNodes (qn1,qn2,Pn1,Pn2);
 dTsB = derivElementCS (qn1,qn2,Pn1,Pn2,TsB);
-d2TsB = secondDerivElementCS (qn1,qn2,Pn1,Pn2,TsB);
-
 mu = getMu (qn1,qn2,Pn1,Pn2,TsB);
 dmu = dmudq (qn1,qn2,Pn1,Pn2,TsB,dTsB);
-d2mu = d2mudq2 (qn1,qn2,Pn1,Pn2,TsB,dTsB,d2TsB);
+
+if (linFlag == 1)
+   d2TsB = secondDerivElementCS (qn1,qn2,Pn1,Pn2,TsB);
+   d2mu = d2mudq2 (qn1,qn2,Pn1,Pn2,TsB,dTsB,d2TsB);
+else
+   d2TsB = sparse(3,3*12*12);
+   d2mu = sparse(12,12*12);
+end
