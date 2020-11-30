8789ba8b26d3a976c8851a97b99d52f6eaca4472
SINTEF-Energy-Wind <karl.merz@sintef.no>
2020-11-30 15:05:44 +0100

    Update parfac.m

--- STAS-Utilities/parfac.m
+++ STAS-Utilities/parfac.m
@@ -1,7 +1,7 @@
 ii = [1:size(shp,1)]';
 jj = ii;
-ss = ones(size(shp,1),1);
-IIshp = sparse(ii,jj,ss,size(shp,1),size(shp,2));
+sss = ones(size(shp,1),1);
+IIshp = sparse(ii,jj,sss,size(shp,1),size(shp,2));
 
 lvec = balanceDiv(shp,IIshp);
 pf = (lvec.').*shp;
@@ -9,8 +9,8 @@ pf = abs(pf);
 
 %ii = [1:Nx]';
 %jj = [1:Nx]';
-%ss = ones(Nx,1);
-%Imat = sparse(ii,jj,ss);
+%sss = ones(Nx,1);
+%Imat = sparse(ii,jj,sss);
 %dvec = pf(Imat == 1)
 %scl = 1./dvec
 %sqmat = sparse(ii,jj,sqrt(scl))
