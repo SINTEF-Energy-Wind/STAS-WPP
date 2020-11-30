fb752aa42338a66e778ebc7ea90f6a921d8859bf
SINTEF-Energy-Wind <karl.merz@sintef.no>
2020-11-30 12:40:41 +0100

    Update globalVelocity.m

--- STAS-Aeroelastic/globalVelocity.m
+++ STAS-Aeroelastic/globalVelocity.m
@@ -31,8 +31,6 @@ function [vg0,Dy] = globalVelocity (qn,qB,Pn,PB,dqndt,dqBdt)
 % vg0             : Nodal velocities at the input conditions.
 % Dy              : dv/dq and dv/dqdot, 6-by-24 for each node.
 
-%id = tic();
-
 Nnod = size (qn,1)/6;
 
 vg0 = zeros(6*Nnod,1);
@@ -40,18 +38,13 @@ Dy  = zeros(6,24*Nnod);
 
 for inod = 1:Nnod
 
-%inod
-%toc(id)
-
    jc24 = 24*(inod-1);
    jc12 = 12*(inod-1);
    jc6 = 6*(inod-1);
    Qg  = Qgnod (qn,qB,Pn,PB);
-%'Qg'
-%toc(id)
+
    dQg = dQgdq (qn,qB,Pn,PB);
-%'dQg'
-%toc(id)
+
    vg0 = Qg*[dqBdt(jc6+[1:6]);dqndt(jc6+[1:6])];
 
    Dy(:,jc24+[13:24]) = Qg;
@@ -63,18 +56,6 @@ for inod = 1:Nnod
       kc12 = 12*(idof+6-1);
       Dy(:,jc24+6+idof) = dQg(:,kc12+[1:12])*[dqBdt(jc6+[1:6]);dqndt(jc6+[1:6])];
    end
-%toc(id)
+
 end
 
-%{
-'---globalVelocity---'
-[qn qB Pn PB dqndt dqBdt]
-Qg
-dqBdt(jc6+[1:6])
-dqndt(jc6+[1:6])
-vg0
-'--end globalVelocity--'
-%}
-
-%'end globalVelocity'
-%toc(id)
